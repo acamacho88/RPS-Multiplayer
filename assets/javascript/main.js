335e0abba55b2533d564c007d7f79be904803ec2
Alex Camacho <alex.c@WalkMe.local>
2018-06-30 12:19:21 -0700

    Initialized all basic files

---
+++ assets/javascript/main.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyDGueBKukCl4V5wirT3D_uNa7XbNroorJE",
+    authDomain: "rps-camacho.firebaseapp.com",
+    databaseURL: "https://rps-camacho.firebaseio.com",
+    projectId: "rps-camacho",
+    storageBucket: "",
+    messagingSenderId: "740913573129"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
